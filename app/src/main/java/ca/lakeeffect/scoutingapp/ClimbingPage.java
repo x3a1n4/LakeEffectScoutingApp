c0416228f791c8a1f027723acc207257f95c94ab
x3a1n4 <billybobbyjoeyyay@gmail.com>
2019-04-06 13:38:47 -0400

    Finished Java for the layouts

--- app/src/main/java/ca/lakeeffect/scoutingapp/ClimbingPage.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/ClimbingPage.java
@@ -11,8 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.GridLayout;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -35,6 +38,64 @@ public class ClimbingPage extends Fragment {
         View view = inflator.inflate(R.layout.climbing_page, container, false);
 
         view.setTag("page5");
+
+        final CheckBox[] sidePreferences = {
+                view.findViewById(R.id.leftSide),
+                view.findViewById(R.id.bothSides),
+                view.findViewById(R.id.rightSide)
+        };
+
+        for(int i = 0; i < sidePreferences.length; i++){
+            final int curr = i;
+            sidePreferences[i].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(b){
+                        for(int i = 0; i < sidePreferences.length; i++){
+                            if(i != curr){
+                                sidePreferences[i].setChecked(false);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        final TextView[] consistencies = {
+                view.findViewById(R.id.middlePlatformConsistency),
+                view.findViewById(R.id.highPlatformConsistency)
+        };
+
+        SeekBar[] seekBars = {
+                view.findViewById(R.id.middlePlatform),
+                view.findViewById(R.id.highPlatform)
+        };
+
+        for(int i = 0; i<seekBars.length; i++){
+            final int curr = i;
+            seekBars[i].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                    final TextView textView = consistencies[curr];
+                    if(progress < 5){
+                        seekBar.setProgress(0);
+                        textView.setText("Can't climb here");
+                    }else{
+                        textView.setText("Consistency (" + progress + "%) :");
+                    }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        }
 
         return view;
 
--- app/src/main/java/ca/lakeeffect/scoutingapp/SandstormPage.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/SandstormPage.java
@@ -1,16 +1,21 @@
 package ca.lakeeffect.scoutingapp;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.Spinner;
@@ -35,6 +40,77 @@ public class SandstormPage extends Fragment {
         View view = inflator.inflate(R.layout.sandstorm_page, container, false);
 
         view.setTag("page4");
+
+        CheckBox[] checkboxes = {
+                view.findViewById(R.id.autoSandstormButton),
+                view.findViewById(R.id.driveSandstormButton),
+                view.findViewById(R.id.highPlatformSandstorm),
+                view.findViewById(R.id.lowPlatformSandstorm),
+                view.findViewById(R.id.cargoPreloadSandstorm),
+                view.findViewById(R.id.hatchPreloadSandstorm)
+        };
+
+        for(int i=0; i<checkboxes.length; i+=2){
+            final CheckBox first = checkboxes[i];
+            final CheckBox second = checkboxes[i+1];
+            first.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(b){
+                        second.setChecked(false);
+                    }
+                }
+            });
+            second.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(b){
+                        first.setChecked(false);
+                    }
+                }
+            });
+        }
+
+        final GridLayout autoGrid = view.findViewById(R.id.autoGrid);
+        final Button addAuto = view.findViewById(R.id.addAuto);
+        final Context context = this.getContext();
+
+        addAuto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int childCount = autoGrid.getChildCount();
+
+                int autoNum = (childCount - 1) / 2 + 1;
+                TextView autoTitle = new TextView(context);
+                autoTitle.setText("Auto " + autoNum + ":");
+                autoTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.text_small)); //TODO: find out why I need to divide by 3
+                autoTitle.setPadding(0, 10, 10, 10);
+                autoTitle.setTypeface(null, Typeface.BOLD);
+
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.setGravity(Gravity.CENTER_VERTICAL);
+                params.setMargins(10, 0, 0, 0);
+                autoTitle.setLayoutParams(params);
+
+                autoGrid.addView(autoTitle, childCount);
+
+                EditText autoDesc = new EditText(context);
+                autoDesc.setHint("Describe auto");
+                //TypeValue.px or something
+                autoDesc.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.text_small)); //TODO: find out why I need to divide by 3
+                params = new GridLayout.LayoutParams();
+                params.setGravity(Gravity.FILL_HORIZONTAL);
+                autoDesc.setLayoutParams(params);
+
+                childCount = autoGrid.getChildCount();
+                autoGrid.addView(autoDesc, childCount);
+
+
+                autoDesc.setWidth(autoDesc.getWidth());
+            }
+        });
+
+
 
         return view;
 
